92e7235daad3eac5f4d41465a7302b7b23d71ce6
Hieu Nguyen <hieunv@live.com>
2023-04-29 16:58:13 +0700

    create project

---
+++ src/main/java/com/sms/api/SrvMntApiApplication.java
@@ -0,0 +1,13 @@
+package com.sms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvMntApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvMntApiApplication.class, args);
+	}
+
+}
